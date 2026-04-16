b81c6c76784d73cd50969a925cf6c13838e88d0d
wanglin <740394554@qq.com>
2026-04-16 18:11:01 +0800

    1

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,4 @@
-export const API_BASE =
-  import.meta.env.VITE_API_BASE ?? 'http://localhost:3000';
+// export const API_BASE =
+//   import.meta.env.VITE_API_BASE ?? 'http://localhost:3000';
+
+export const API_BASE = 'http://81.71.24.66';
